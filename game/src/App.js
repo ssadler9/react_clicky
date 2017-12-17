454e4ae2592e0520349e0dae00de7693adcbee82
Sam Sadler <samsadler@sams-mbp.attlocal.net>
2017-12-17 12:29:13 -0600

    lifecycle events added.  clicks not registering

--- game/src/App.js
+++ game/src/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Jumbotron from "./components/jumbotron";
 import Tile from "./components/tile";
+
 // Game code goes in App
 
-class App extends Component {
-  render() {
-    return (
+const App = () =>
       <div className="container">
         <Jumbotron />
         <Tile />
-      </div>
-    );
-  }
-}
+      </div>;
 
 export default App;
--- game/src/components/tile.js
+++ game/src/components/tile.js
@@ -1,78 +1,113 @@
-import React from "react";
+import React, { Component } from "react";
+
 // The pictures that the user will click on will go here.
-// I will need 12 pictures (tiles) total - 4 rows of 3
-const Tile = () => (
-	<div>
-		<div className="row">
-			<div className="col-sm-3">
-				<div className="card" style={{ width: '20rem' }}>
-					<img className="card-img-top" src="../../public/assets/pictures/goku.jpg" alt="Goku" />
-					<div className="card-body">
+// I will need 12 pictures (tiles) total - 4 rows of 4
+
+class Tile extends Component {
+
+	state = {
+		clicked: false,
+		score: 0
+	};
+
+	handleInputChange = event => {
+
+
+		if(this.state.clicked === event.target) {
+			this.setState({
+				clicked : true
+			});
+		};
+		console.log(this.state.clicked);
+
+		
+	};
+
+	render () {
+		return (
+			<div>
+				<h1>Score: {this.state.score}</h1>
+				<div className="row">
+					<div className="col-sm-4">
+						<div className="card" style={{ width: '20rem;' }}>
+							<img className="card-img-top" src="../public/assets/pictures/goku.jpg" alt="Goku" />
+							<div className="card-body">
+							<button className="btn btn-primary" type="submit" value={this.state.clicked}>Goku</button>
+							</div>
+						</div>
+					</div>
+					<div className="col-sm-4">
+						<div className="card" style={{ width: '20rem;' }}>
+							<img className="card-img-top" src="../../public/assets/pictures/goku.jpg" alt="Gohan" />
+							<div className="card-body">
+							<button className="btn btn-primary" type="submit" value={this.state.clicked}>Gohan</button>
+							</div>
+						</div>
+					</div>
+					<div className="col-sm-4">
+						<div className="card" style={{ width: '20rem;' }}>
+							<img className="card-img-top" src="../../public/assets/pictures/vegeta.jpg" alt="Vegeta" />
+							<div className="card-body">
+							<button className="btn btn-primary" type="submit" value={this.state.clicked}>Vegeta</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-4">
+						<div className="card" style={{ width: '20rem;' }}>
+							<img className="card-img-top" src="../../public/assets/pictures/piccolo.jpg" alt="Piccolo" />
+							<div className="card-body">
+							<button className="btn btn-primary" type="submit" value={this.state.clicked}>Piccolo</button>
+							</div>
+						</div>
+					</div>
+					<div className="col-sm-4">
+						<div className="card" style={{ width: '20rem;' }}>
+							<img className="card-img-top" src="../../public/assets/pictures/roshi.jpg" alt="Master Roshi" />
+							<div className="card-body">
+							<button className="btn btn-primary" type="submit" value={this.state.clicked}>Master Roshi</button>
+							</div>
+						</div>
+					</div>
+					<div className="col-sm-4">
+						<div className="card" style={{ width: '20rem;' }}>
+							<img className="card-img-top" src="../../public/assets/pictures/beerus.jpg" alt="Lord Beerus" />
+							<div className="card-body">
+							<button className="btn btn-primary" type="submit" value={this.state.clicked}>Lord Beerus</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-4">
+						<div className="card" style={{ width: '20rem;' }}>
+							<img className="card-img-top" src="../../public/assets/pictures/17.jpg" alt="#17" />
+							<div className="card-body">
+							<button className="btn btn-primary" type="submit" value={this.state.clicked}>Android 17</button>
+							</div>
+						</div>
+					</div>
+					<div className="col-sm-4">
+						<div className="card" style={{ width: '20rem;' }}>
+							<img className="card-img-top" src="../../public/assets/pictures/18.jpg" alt="#18" />
+							<div className="card-body">
+							<button className="btn btn-primary" type="submit" value={this.state.clicked}>Android 18</button>
+							</div>
+						</div>
+					</div>
+					<div className="col-sm-4">
+						<div className="card" style={{ width: '20rem;' }}>
+							<img className="card-img-top" src="../public/assets/pictures/krillin.png" alt="Krillin" />
+							<div className="card-body">
+							<button className="btn btn-primary" type="submit" value={this.state.clicked}>Krillin</button>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div className="col-sm-3">
-				<div className="card" style={{ width: '20rem' }}>
-					<img className="card-img-top" src="../../public/assets/pictures/goku.jpg" alt="Gohan" />
-					<div className="card-body">
-					</div>
-				</div>
-			</div>
-			<div className="col-sm-3">
-				<div className="card" style={{ width: '20rem' }}>
-					<img className="card-img-top" src="../../public/assets/pictures/vegeta.jpg" alt="Vegeta" />
-					<div className="card-body">
-					</div>
-				</div>
-			</div>
-		</div>
-		<div className="row">
-			<div className="col-sm-3">
-				<div className="card" style={{ width: '20rem' }}>
-					<img className="card-img-top" src="../../public/assets/pictures/piccolo.jpg" alt="Piccolo" />
-					<div className="card-body">
-					</div>
-				</div>
-			</div>
-			<div className="col-sm-3">
-				<div className="card" style={{ width: '20rem' }}>
-					<img className="card-img-top" src="../../public/assets/pictures/roshi.jpg" alt="Master Roshi" />
-					<div className="card-body">
-					</div>
-				</div>
-			</div>
-			<div className="col-sm-3">
-				<div className="card" style={{ width: '20rem' }}>
-					<img className="card-img-top" src="../../public/assets/pictures/beerus.jpg" alt="Lord Beerus" />
-					<div className="card-body">
-					</div>
-				</div>
-			</div>
-		</div>
-		<div className="row">
-			<div className="col-sm-3">
-				<div className="card" style={{ width: '20rem' }}>
-					<img className="card-img-top" src="../../public/assets/pictures/17.jpg" alt="#17" />
-					<div className="card-body">
-					</div>
-				</div>
-			</div>
-			<div className="col-sm-3">
-				<div className="card" style={{ width: '20rem' }}>
-					<img className="card-img-top" src="../../public/assets/pictures/18.jpg" alt="#18" />
-					<div className="card-body">
-					</div>
-				</div>
-			</div>
-			<div className="col-sm-3">
-				<div className="card" style={{ width: '20rem' }}>
-					<img className="card-img-top" src="../../public/assets/pictures/krillin.png" alt="Krillin" />
-					<div className="card-body">
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-);
+		);
+	}
+} 
 
 export default Tile;
